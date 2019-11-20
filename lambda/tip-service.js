e2926eb2a3900f7b7ed8fbf81ef37ebc8023b8a0
OK Rabbit <ocrybit@gmail.com>
2019-11-20 01:53:57 +0100

    speed up with parallel executions

--- lambda/tip-service.js
+++ lambda/tip-service.js
@@ -1,6 +1,7 @@
 const path = require("path")
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
 const { parse } = require("url")
+const R = require("ramda")
 const alis = require("alis")
 const DB = require("monk")(process.env.MONGODB_URL)
 const db = {
@@ -18,49 +19,66 @@ module.exports = async (req, res) => {
   let actions_executed = []
   const { query } = parse(req.url, true)
   try {
-    let json = await alis.p.me.notifications({ limit: 100 }, cred())
-    let tips_received = {}
-    for (let v of json.Items) {
-      if (v.type === "tip") {
+    const tips = R.compose(
+      R.map(v => {
         v.tip_value_normalized = v.tip_value / tip_divider
-        console.log(
-          `[${v.article_id}] ${v.tip_value_normalized} ALIS from ${
-            v.acted_user_id
-          }`
-        )
-        if (typeof tips_received[v.article_id] === "undefined") {
-          tips_received[v.article_id] = []
-        }
-        tips_received[v.article_id].push(v)
-        if (typeof actions[v.article_id] !== "undefined") {
-          let id = `${v.acted_user_id}@${v.created_at}`
-          if (typeof tipped[v.article_id] === "undefined") {
-            let doc = (await db.tips.findOne({
-              article_id: v.article_id
-            })) || {
-              history: {}
-            }
-            tipped[v.article_id] = doc.history
-          }
-          if (typeof tipped[v.article_id][id] === "undefined") {
-            let action = await actions[v.article_id](v)
-            if (action === true) {
-              let executed_at = Date.now()
-              tipped[v.article_id][id] = executed_at
-              actions_executed.push({
-                notification: v,
-                executed_at: executed_at
-              })
-              await db.tips.update(
-                { article_id: v.article_id },
-                { article_id: v.article_id, history: tipped[v.article_id] },
-                { upsert: true }
-              )
-            }
-          }
-        }
-      }
+        return v
+      }),
+      R.filter(v => !R.isNil(actions[v.article_id])),
+      R.filter(R.propEq("type", "tip")),
+      R.prop("Items")
+    )(await alis.p.me.notifications({ limit: 100 }, cred()))
+    const article_ids = R.compose(
+      R.uniq,
+      R.pluck("article_id")
+    )(tips)
+    let histories = R.compose(
+      R.map(R.prop("history")),
+      R.indexBy(R.prop("article_id"))
+    )(
+      await db.tips.find({
+        article_id: { $in: article_ids }
+      })
+    )
+    for (let id of R.difference(article_ids)(R.keys(histories))) {
+      histories[id] = {}
     }
+    const promises = R.compose(
+      R.addIndex(R.map)((v, i) => {
+        return {
+          index: i,
+          tip: v,
+          promise: actions[v.article_id](v)
+        }
+      }),
+      R.filter(v => {
+        return R.isNil(
+          histories[v.article_id][`${v.acted_user_id}@${v.created_at}`]
+        )
+      })
+    )(tips)
+    const results = await Promise.all(R.pluck("promise")(promises))
+    const executed_at = Date.now()
+    const updates = R.compose(
+      R.map(v => {
+        const id = `${v.tip.acted_user_id}@${v.tip.created_at}`
+        histories[v.tip.article_id][id] = executed_at
+        actions_executed.push({
+          notification: v.tip,
+          executed_at: executed_at
+        })
+        return db.tips.update(
+          { article_id: v.tip.article_id },
+          {
+            article_id: v.tip.article_id,
+            history: histories[v.tip.article_id]
+          },
+          { upsert: true }
+        )
+      }),
+      R.filter(v => results[v.index])
+    )(promises)
+    await Promise.all(updates)
     res.end(JSON.stringify(actions_executed))
   } catch (e) {
     console.log(e)
